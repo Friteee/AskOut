3b6dd99e6d588cb11651f1410b4ceae2daf3d61c
Nikita <fritee@mail.ru>
2017-03-11 03:02:54 +0000

    Made getter for location from IP and GPS. Started working on php back-end

---
+++ php/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = 'root';
+$db_name = 'plsnokill';
+$db_port = '8889';
+
+$api_secret = 'e7eagk+/dd4CfmS/L4o8l7n6FCsnwxydAz3wFOwyk/k=';
