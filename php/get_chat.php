6c9970cb9b55c2c6f9d4d69fb20214e46dc38fba
Ilias Akhmedov <mrnoone38@gmail.com>
2017-03-12 00:28:56 +0000

    Add Service SID

--- php/get_chat.php
+++ php/get_chat.php
@@ -6,5 +6,5 @@ use Twilio\Rest\Client;
 $sid = "ACa0c7fb496a58af7fcc32e8c5e41bbe2e";
 $token = "e13175ce8269342d9c0c41168f4ce593";
 $client = new Client($sid, $token);
-
+$SERVICE_SID = "IS9aa8b1a079d8434bbea625fc649b1461";
 ?>
